f93fb67fe73fd277adb2605125d0d70070e6072d
sadiuchiha <sadiuchiha@gmail.com>
2021-09-14 08:19:30 +0600

    Added frontend firstPage
    This firstPage will be SigninView

--- frontend/chat-app/src/App.js
+++ frontend/chat-app/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <div className="grid-container">
+          <header className="row">
+            <div>
+              <Link className="brand" to="/">
+                ChatZ
+              </Link>
+            </div>
+          </header>
+
+          <footer className="row center">All Rights reserved</footer>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
